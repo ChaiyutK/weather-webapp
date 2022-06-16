95c4c3f6acf9806fd242ebdb81f899ede4fc8274
Chaiyut <benzcyber53@gmail.com>
2022-06-16 16:13:07 +0700

    add animation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  AOS.init({duration: 2000});
   const [data,setData] = useState({});
-  const [city,setCity] = useState("bangkok");
+  const [city,setCity] = useState("");
   const [date,setDate] = useState("");
   
 
@@ -13,7 +16,14 @@ function App() {
     if(e.key === "Enter")
     {
       getData();
-      
+      setCity("");
+      if ( document.getElementById("tempid").classList.contains('tempactive') )
+      {
+        document.getElementById("tempid").classList.remove('tempactive')
+      }
+      else{
+        document.getElementById("tempid").classList.add('tempactive')
+      }
     }
   
   }
@@ -33,15 +43,14 @@ function App() {
     <div className={typeof data.name != "undefined" && data.main.temp > 20 ? "App hot" : typeof data.name != "undefined" && data.main.temp < 12 ? "App cold" : "App"}>
       <div className='weather-box'>
         <div className="weather-item">
-        <input placeholder='Enter City Name' type="text" onChange={(event) => {setCity(event.target.value)}} onKeyPress={search} />
+        <input placeholder='Enter City Name' type="text" onChange={(event) => {setCity(event.target.value)}} value={city} onKeyPress={search} />
         </div>
-      
       {(typeof data.name != "undefined" ? (
         <>
-        <div className="weather-item"><h2 className='cityname'>{data.name},{data.sys.country}</h2></div>
-          <div className="weather-item"><h2 className='date'>{date}</h2></div>
-          <div className="weather-item"><h2 className='temp'>{data.main.temp} °C</h2></div>
-          <div className="weather-item"><h2 className='cloud'>{data.weather[0].main}</h2></div>
+        <div className="weather-item" data-aos="flip-up"><h2 className='cityname'>{data.name},{data.sys.country}</h2></div>
+          <div className="weather-item" data-aos="flip-up"><h2 className='date'>{date}</h2></div>
+          <div className="weather-item" data-aos="flip-up"><h2 id='tempid' className='temp'>{data.main.temp} °C</h2></div>
+          <div className="weather-item" data-aos="flip-up"><h2 className='cloud'>{data.weather[0].main}</h2></div>
         </>
       ) : "")}
       </div>
